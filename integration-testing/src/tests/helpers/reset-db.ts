2a9eb38997c166e0ace15fd7bc10e44a8cecfd44
Rakesh Hemwani <64716996+rakesh-hemwani@users.noreply.github.com>
2024-07-22 17:24:02 +0530

    Update reset-db.ts

--- integration-testing/src/tests/helpers/reset-db.ts
+++ integration-testing/src/tests/helpers/reset-db.ts
@@ -2,6 +2,9 @@ import { PrismaClient} from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+
+
+
 export async function clearDb () {
     await prisma.$transaction([
         prisma.request.deleteMany(),
